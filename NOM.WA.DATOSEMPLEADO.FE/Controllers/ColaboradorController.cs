6ff497e93c74b651f04603043d6c8bf165684708
Vidal Chay <vidal.chay@CHICOTE.local>
2025-03-21 14:07:36 -0600

    sube proyecto

---
+++ NOM.WA.DATOSEMPLEADO.FE/Controllers/ColaboradorController.cs
@@ -0,0 +1,120 @@
+﻿using NOM.WA.DATOSEMPLEADO.FE.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace NOM.WA.DATOSEMPLEADO.FE.Controllers
+{
+    public class ColaboradorController : Controller
+    {
+        // GET: Colaborador
+        private readonly string apiUrl = "https://localhost:7052/api/Colaborador";
+
+        // GET: Colaborador
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public async Task<ActionResult> ObtenerColaboradores()
+        {
+            List<Colaborador> colaboradores = new List<Colaborador>();
+
+            ServicePointManager.ServerCertificateValidationCallback = (sender, cert, chain, sslPolicyErrors) => true;
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string responseContent = await response.Content.ReadAsStringAsync();
+                        colaboradores = JsonConvert.DeserializeObject<List<Colaborador>>(responseContent);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ViewBag.ErrorMessage = $"Error: {ex.Message}";
+                }
+            }
+
+            return Json(colaboradores, JsonRequestBehavior.AllowGet);
+        }
+
+        // GET: Colaborador/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Colaborador/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Colaborador/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Colaborador/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Colaborador/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Colaborador/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Colaborador/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ NOM.WA.DATOSEMPLEADO.FE/Models/Colaborador.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NOM.WA.DATOSEMPLEADO.FE.Models
+{
+    public class Colaborador
+    {
+        public int IdColaborador { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Direccion { get; set; }
+        public int Edad { get; set; }
+        public string Profesion { get; set; }
+        public string Estado_Civil { get; set; }
+    }
+}
